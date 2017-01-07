fc275d2263902c54763b495c46ea083a8c1de411
Your name <you@example.com>
2017-01-07 17:27:00 +0530

    SessionStorage

--- public/app/product/product.services.js
+++ public/app/product/product.services.js
@@ -1,12 +1,26 @@
 angular.module("product.services", [])
 
-    .service("productService", function ($http, apiEndPoint) {
+    .service("productService", function ($http, apiEndPoint, $q) {
         console.log("productservice created");
 
         this.getProducts = function () {
+
+            // Caching Session Storage - Not available per tab basis
+            var storage = window.sessionStorage;
+            // Synchronous call, so we injected $q at service level, so our cpntroller wont break
+            var productData = storage.getItem("products");
+            if (productData) {
+                console.log("serving from cache");
+                return $q.resolve(JSON.parse(productData));
+            }
+
+            console.log("serving from server");
+
             return $http
                 .get(apiEndPoint + "/api/products")
                 .then(function (response) {
+                    console.log("cache updated");
+                    storage.setItem("products", JSON.stringify(response.data)); // stores in storage
                     return response.data;
                 })
         }
@@ -30,5 +44,5 @@ angular.module("product.services", [])
 
     .factory("Brand", function ($resource, apiEndPoint) {
         var Brand = $resource(apiEndPoint + "/api/brands/:id");
-        return Brand;                   
+        return Brand;
     })
